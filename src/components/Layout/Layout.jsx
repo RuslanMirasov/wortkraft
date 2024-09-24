318b769f6760c82c4d765efb8d05eca968644cee
Ruslan <info@mirasov.dev>
2024-09-24 16:06:39 +0200

    fix bugs with path and symbols

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,28 +1,36 @@
 import Link from "next/link";
+import Head from "next/head";
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>WortKraft</title>
+        <meta
+          name="description"
+          content="Web application for learning German words from current German textbooks in private and general education."
+        />
+      </Head>
       <header>
         <b>Logo</b>
         <nav>
           <ul>
             <li>
-              <Link href="./">Books</Link>
+              <Link href="/">Books</Link>
             </li>
             <li>
-              <Link href="./search">Search</Link>
+              <Link href="/search">Search</Link>
             </li>
             <li>
-              <Link href="./bookmarks">Bookmarks</Link>
+              <Link href="/bookmarks">Bookmarks</Link>
             </li>
             <li>
-              <Link href="./statistic">Statistic</Link>
+              <Link href="/statistic">Statistic</Link>
             </li>
           </ul>
         </nav>
         <div>
-          <Link href="./profile">Profile</Link>
+          <Link href="/profile">Profile</Link>
         </div>
       </header>
       <main>{children}</main>
