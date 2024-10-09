3ded2ab5d4810b82750000e78d7a9f4fa67f6ae3
Ruslan <info@mirasov.dev>
2024-10-09 12:48:05 +0200

    getDifference function fix

--- src/components/LerningRoom/WriteForm.jsx
+++ src/components/LerningRoom/WriteForm.jsx
@@ -13,14 +13,43 @@ const findAllIndexes = (str, letter) => {
 };
 
 function getDifference(str1, str2) {
-  let minLength = Math.min(str1.length, str2.length);
-  let index = 0;
+  let diff = '';
+  const charCountStr1 = {};
+  const charCountStr2 = {};
 
-  while (index < minLength && str1[index] === str2[index]) {
-    index++;
+  // Считаем количество каждого символа в str1
+  for (let char of str1) {
+    charCountStr1[char] = (charCountStr1[char] || 0) + 1;
   }
 
-  return str2.slice(index);
+  // Считаем количество каждого символа в str2
+  for (let char of str2) {
+    charCountStr2[char] = (charCountStr2[char] || 0) + 1;
+  }
+
+  // Сравниваем количество символов в str2 с количеством в str1
+  for (let char in charCountStr2) {
+    const countInStr1 = charCountStr1[char] || 0;
+    const countInStr2 = charCountStr2[char];
+
+    // Если в str2 больше символов, добавляем их к разнице
+    if (countInStr2 > countInStr1) {
+      diff += char.repeat(countInStr2 - countInStr1);
+    }
+  }
+
+  // Сравниваем количество символов в str1 с количеством в str2 для учета удалённых символов
+  for (let char in charCountStr1) {
+    const countInStr1 = charCountStr1[char];
+    const countInStr2 = charCountStr2[char] || 0;
+
+    // Если в str1 больше символов, добавляем их к разнице
+    if (countInStr1 > countInStr2) {
+      diff += char.repeat(countInStr1 - countInStr2);
+    }
+  }
+
+  return diff;
 }
 
 const WriteForm = ({ name }) => {
@@ -45,6 +74,7 @@ const WriteForm = ({ name }) => {
     if (value.length < newValue.length) {
       // КОГДА ПИШЕМ ТЕКСТ
       const newLetters = getDifference(value, newValue);
+      console.log(newLetters);
       newLetters.split('').forEach(letter => {
         const allIndexes = findAllIndexes(lettersArr, letter);
 
@@ -57,25 +87,26 @@ const WriteForm = ({ name }) => {
       });
     } else {
       // КОГДА СТИРАЕМ ТЕКСТ
-      const newLetters = getDifference(newValue, value);
-      if (newLetters.length > 1) {
+      const newLetters = getDifference(value, newValue);
+      if (newValue === '') {
         setDisabledLetters({});
-      } else {
-        newLetters.split('').forEach(letter => {
-          const allIndexes = findAllIndexes(lettersArr, letter);
-          console.log(allIndexes);
-          for (const index of allIndexes) {
-            if (disabledLetters[index]) {
-              setDisabledLetters(prevState => {
-                const updatedState = { ...prevState };
-                delete updatedState[index];
-                return updatedState;
-              });
-              break;
-            }
-          }
-        });
+        setValue(newValue);
+        return;
       }
+      console.log(newLetters);
+      newLetters.split('').forEach(letter => {
+        const allIndexes = findAllIndexes(lettersArr, letter);
+        for (const index of allIndexes) {
+          if (disabledLetters[index]) {
+            setDisabledLetters(prevState => {
+              const updatedState = { ...prevState };
+              delete updatedState[index];
+              return updatedState;
+            });
+            break;
+          }
+        }
+      });
     }
 
     setValue(newValue);
